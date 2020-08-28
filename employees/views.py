5509fa897dcff9db92849c5bdd9c9ecbfd91ff4d
Re <refanopradana@gmail.com>
2020-08-28 19:23:56 +0700

    03 Working with url

--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return HttpResponse('Welcome Home')
+
+
+def employee_detail(request, employee_id):
+    return HttpResponse(f'Employee ID: {employee_id}')
